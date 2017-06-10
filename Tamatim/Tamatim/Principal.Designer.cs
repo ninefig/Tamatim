5289c80d61aea2bf91be0ba8a79c6b793c5c1ae2
Wilton <wilton-barbosa@hotmail.com>
2017-06-10 05:59:56 -0300

    Finalizado

--- Tamatim/Tamatim/Principal.Designer.cs
+++ Tamatim/Tamatim/Principal.Designer.cs
@@ -597,7 +597,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(526, 220);
+            this.label6.Location = new System.Drawing.Point(526, 210);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(58, 24);
             this.label6.TabIndex = 17;
